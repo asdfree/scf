d2066e2ddd07885c845417e57a6ac4f468b47ecb
Anthony Damico <ajdamico@gmail.com>
2017-07-22 06:28:17 -0400

    try three

---
+++ tests/setup.R
@@ -0,0 +1,13 @@
+machine_specific_replacements <- 
+	list( 
+		
+		# replace the folder path on macnix
+		c( 'path.expand( \"~\" ) , \"SCF\"' , paste0( '"' , getwd() , '"' ) ) ,
+		
+		# change other things in the script to be run
+		c( "hello" , "howdy" )
+		
+	)
+
+source( lodown::syntaxtractor( "scf" , replacements = machine_specific_replacements , setup_test = "setup" ) , echo = TRUE )
+
