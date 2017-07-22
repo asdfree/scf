1a5d6f036f0c3270ce277dbf3929e3f80bfcaa6a
Anthony Damico <ajdamico@gmail.com>
2017-07-22 16:51:00 -0400

    setup automation

--- tests/setup.R
+++ tests/setup.R
@@ -1,12 +1,4 @@
-machine_specific_replacements <- 
-	list( 
-		
-		# replace the folder path on macnix
-		c( 'path.expand( \"~\" ) , \"SCF\"' , paste0( '"' , getwd() , '"' ) ) ,
-		
-		# change other things in the script to be run
-		c( "hello" , "howdy" )
-	)
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
-source( lodown::syntaxtractor( "scf" , replacements = machine_specific_replacements , setup_test = "setup" ) , echo = TRUE )
+library(lodown)
+lodown( "scf" , output_dir = file.path( getwd() ) )
