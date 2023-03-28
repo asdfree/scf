09faa039f850145ed3369606128b0b00ac05eb93
Anthony Damico <ajdamico@gmail.com>
2023-03-28 03:06:13 -0400

    scf draft

--- tests/setup.R
+++ tests/setup.R
@@ -1,9 +1,6 @@
 # incomes, assets, debts
 # high net worth oversample
 # pig bank laproscope
-
-# variant of code{mitools::scf_MIcombine} that only uses the sampling variance from the first implicate instead of averaging all five
-
 scf_MIcombine <-
 	function (results, variances, call = sys.call(), df.complete = Inf, ...) {
 		m <- length(results)
@@ -39,110 +36,65 @@ scf_MIcombine <-
 		class(rval) <- "MIresult"
 		rval
 	}
-
 library(haven)
 
-tf_s <- tempfile()
+scf_dta_import <-
+	function( this_url ){
+		
+		this_tf <- tempfile()
+		
+		download.file( this_url , this_tf , mode = 'wb' )
+		
+		this_tbl <- read_dta( this_tf )
+		
+		this_df <- data.frame( this_tbl )
+		
+		file.remove( this_file )
+		
+		names( this_df ) <- tolower( names( this_df ) )
+		
+		this_df
+	}
 
-tf_p <- tempfile()
+scf_df <- scf_dta_import( "https://www.federalreserve.gov/econres/files/scf2019s.zip" )
 
-tf_rw <- tempfile()
+ext_df <- scf_dta_import( "https://www.federalreserve.gov/econres/files/scfp2019s.zip" )
 
-download.file( "https://www.federalreserve.gov/econres/files/scf2019s.zip" , tf_s , mode = 'wb' )
+scf_rw_df <- scf_dta_import( "https://www.federalreserve.gov/econres/files/scf2019rw1s.zip" )
 
-download.file( "https://www.federalreserve.gov/econres/files/scfp2019s.zip" , tf_p , mode = 'wb' )
+stopifnot( nrow( scf_df ) == nrow( scf_rw_df ) * 5 )
+stopifnot( nrow( scf_df ) == nrow( ext_df ) )
+stopifnot( all( sort( intersect( names( scf_df ) , names( ext_df ) ) ) == c( 'y1' , 'yy1' ) ) )
+stopifnot( all( sort( intersect( names( scf_df ) , names( scf_rw_df ) ) ) == c( 'y1' , 'yy1' ) ) )
+stopifnot( all( sort( intersect( names( ext_df ) , names( scf_rw_df ) ) ) == c( 'y1' , 'yy1' ) ) )
+scf_rw_df[ , 'y1' ] <- NULL
 
-download.file( "https://www.federalreserve.gov/econres/files/scf2019rw1s.zip" , tf_rw , mode = 'wb' )
-
-s_tbl <- read_dta( tf_s )
-
-p_tbl <- read_dta( tf_p )
-
-rw_tbl <- read_dta( tf_rw )
-
-s_df <- data.frame( s_tbl )
-
-p_df <- data.frame( p_tbl )
-
-scf_rw_df <- data.frame( rw_tbl )
-
-names( s_df ) <- tolower( names( s_df ) )
-
-names( p_df ) <- tolower( names( p_df ) )
-
-names( scf_rw_df ) <- tolower( names( scf_rw_df ) )
-
-stopifnot( nrow( s_df ) == nrow( scf_rw_df ) * 5 )
-stopifnot( nrow( s_df ) == nrow( p_df ) )
+scf_df[ , 'five' ] <- 5
 # scf_fn <- file.path( path.expand( "~" ) , "SCF" , "this_file.rds" )
 # saveRDS( scf_df , file = scf_fn , compress = FALSE )
 # scf_df <- readRDS( scf_fn )
-library(survey)
-library(mitools)
+library(stringr)
 
-# confirm that the only overlapping columns
-# between the three data sets are `y1`
-# (the unique primary economic unit id - peu)
-# and `yy1` (the five records of the peu)
-stopifnot( all.equal( sort( intersect( names( s_df ) , names( p_df ) ) ) , c( 'y1' , 'yy1' ) ) )
-stopifnot( all.equal( sort( intersect( names( s_df ) , names( scf_rw_df ) ) ) , c( 'y1' , 'yy1' ) ) )
-stopifnot( all.equal( sort( intersect( names( p_df ) , names( scf_rw_df ) ) ) , c( 'y1' , 'yy1' ) ) )
-
-# throw out the unique identifiers ending with `1`
-# because they only match one-fifth of the records in the survey data
-scf_rw_df$y1 <- NULL
-
-# `s_df` currently contains
-# five records per household -- all five of the implicates.
-
-# add a column `one` to every record, containing just the number one
-s_df$one <- 1
-
-# add a column `five` to every record, containing just the number five
-s_df$five <- 5
-# note: this column should be used to calculate weighted totals.
-
-# break `s_df` into five different data sets
-# based on the final character of the column 'y1'
-# which separates the five implicates
-s1_df <- s_df[ substr( s_df$y1 , nchar( s_df$y1 ) , nchar( s_df$y1 ) ) == 1 , ]
-s2_df <- s_df[ substr( s_df$y1 , nchar( s_df$y1 ) , nchar( s_df$y1 ) ) == 2 , ]
-s3_df <- s_df[ substr( s_df$y1 , nchar( s_df$y1 ) , nchar( s_df$y1 ) ) == 3 , ]
-s4_df <- s_df[ substr( s_df$y1 , nchar( s_df$y1 ) , nchar( s_df$y1 ) ) == 4 , ]
-s5_df <- s_df[ substr( s_df$y1 , nchar( s_df$y1 ) , nchar( s_df$y1 ) ) == 5 , ]
-
-# count the total number of records in `s_df`
-m.rows <- nrow( s_df )
-
-# confirm that the number of records did not change
-stopifnot(
-	sum( nrow( s1_df ) , nrow( s2_df ) , nrow( s3_df ) , nrow( s4_df ) , nrow( s5_df ) ) == m.rows
-)
-
-# sort all five implicates by the unique identifier
-# s1_df <- s1_df[ order( s1_df$yy1 ) , ]
-# s2_df <- s2_df[ order( s2_df$yy1 ) , ]
-# s3_df <- s3_df[ order( s3_df$yy1 ) , ]
-# s4_df <- s4_df[ order( s4_df$yy1 ) , ]
-# s5_df <- s5_df[ order( s5_df$yy1 ) , ]
-
+s1_df <- scf_df[ str_sub( scf_df[ , 'y1' ] , -1 , -1 ) == 1 , ]
+s2_df <- scf_df[ str_sub( scf_df[ , 'y1' ] , -1 , -1 ) == 2 , ]
+s3_df <- scf_df[ str_sub( scf_df[ , 'y1' ] , -1 , -1 ) == 3 , ]
+s4_df <- scf_df[ str_sub( scf_df[ , 'y1' ] , -1 , -1 ) == 4 , ]
+s5_df <- scf_df[ str_sub( scf_df[ , 'y1' ] , -1 , -1 ) == 5 , ]
 scf_imp <- list( s1_df , s2_df , s3_df , s4_df , s5_df )
 
-scf_list <- lapply( scf_imp , merge , p_df )
+scf_list <- lapply( scf_imp , merge , ext_df )
 
-scf_list <- lapply( scf_list , function( w ) w[ order( w[ , 'yy1' ] ) , ] )
-
-# replace all missing values in the replicate weights table with zeroes..
 scf_rw_df[ is.na( scf_rw_df ) ] <- 0
 
-# ..then multiply the replicate weights by the multiplication factor
-scf_rw_df[ , paste0( 'wgt' , 1:999 ) ] <- scf_rw_df[ , paste0( 'wt1b' , 1:999 ) ] * scf_rw_df[ , paste0( 'mm' , 1:999 ) ]
+scf_rw_df[ , paste0( 'wgt' , 1:999 ) ] <-
+	scf_rw_df[ , paste0( 'wt1b' , 1:999 ) ] * scf_rw_df[ , paste0( 'mm' , 1:999 ) ]
 
-# only keep the unique identifier and the final (combined) replicate weights
 scf_rw_df <- scf_rw_df[ , c( 'yy1' , paste0( 'wgt' , 1:999 ) ) ]
+scf_list <- lapply( scf_list , function( w ) w[ order( w[ , 'yy1' ] ) , ] )
 
-# sort the replicate weights data frame by the unique identifier as well
 scf_rw_df <- scf_rw_df[ order( scf_rw_df$yy1 ) , ]
+library(survey)
+library(mitools)
 
 scf_design <- 
 	svrepdesign( 
@@ -160,13 +112,14 @@ scf_design <-
 	update( 
 		scf_design , 
 		
-		hhsex = factor( hhsex , labels = c( "male" , "female" ) ) ,
+		hhsex = factor( hhsex , levels = 1:2 , labels = c( "male" , "female" ) ) ,
 		
 		married = as.numeric( married == 1 ) ,
 		
 		edcl = 
 			factor( 
 				edcl , 
+				levels = 1:4 ,
 				labels = 
 					c( 
 						"less than high school" , 
@@ -177,13 +130,13 @@ scf_design <-
 			)
 
 	)
-scf_MIcombine( with( scf_design , svyby( ~ one , ~ one , unwtd.count ) ) )
+scf_MIcombine( with( scf_design , svyby( ~ five , ~ one , unwtd.count ) ) )
 
-scf_MIcombine( with( scf_design , svyby( ~ one , ~ hhsex , unwtd.count ) ) )
-scf_MIcombine( with( scf_design , svytotal( ~ one ) ) )
+scf_MIcombine( with( scf_design , svyby( ~ five , ~ hhsex , unwtd.count ) ) )
+scf_MIcombine( with( scf_design , svytotal( ~ five ) ) )
 
 scf_MIcombine( with( scf_design ,
-	svyby( ~ one , ~ hhsex , svytotal )
+	svyby( ~ five , ~ hhsex , svytotal )
 ) )
 scf_MIcombine( with( scf_design , svymean( ~ networth ) ) )
 
@@ -276,7 +229,7 @@ stopifnot( abs( 15.6 - round( SE( mean_net_worth ) / 1000 , 1 ) ) < 0.1 )
 
 # compute quantile with all five implicates stacked
 # matches the public excel tables but not the recommended technique
-fake_design <- svydesign( ~ 1 , data = p_df[ c( 'networth' , 'wgt' ) ] , weights = ~ wgt )
+fake_design <- svydesign( ~ 1 , data = ext_df[ c( 'networth' , 'wgt' ) ] , weights = ~ wgt )
 median_net_worth_incorrect_errors <- svyquantile( ~ networth , fake_design , 0.5 )
 
 # exactly match "Table 4" tab cell V6 of
